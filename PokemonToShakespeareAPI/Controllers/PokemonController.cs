d6f1b8d6cdd0cbd5fa577b0fbac121d5c26fe7ea
andrea bolzoni <bolzo5@gmail.com>
2020-05-24 10:44:50 +0200

    first commit, logic in controller

---
+++ PokemonToShakespeareAPI/Controllers/PokemonController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using PokemonToShakespeareAPI.Services;
+
+namespace PokemonToShakespeareAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public PokemonController(IConfiguration config)
+        {
+            _config = config;
+        }
+        // GET: api/Pokemon
+        [HttpGet]
+        public string Get()
+        {
+            string message= "Please call this API passing a pokemon name as parameter, example: pokemon/charizard";
+            return message;
+        }
+
+        // GET: api/Pokemon/name
+        [HttpGet("{Name}", Name = "Get")]
+        public string Get(string Name)
+        {
+            string textLanguage=_config.GetValue<string>(
+                "PokemonAPIOptions:Language");
+
+            Task<string> task = getPokemonDescriptionAsync(Name.ToLower(),textLanguage);
+            task.Wait();
+            string name = task.Result;
+            return name;
+        }
+
+        
+        public async Task<string> getPokemonDescriptionAsync(string Name,string textLanguage)
+        {
+
+            int id = 0;
+            string des = "";
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://pokeapi.co/api/v2/pokemon");
+                //HTTP GET
+                var responseTask = client.GetAsync("pokemon/" + Name);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = await result.Content.ReadAsStringAsync();
+                    //name = readTask;
+                    Pokemon pk = JsonSerializer.Deserialize<Pokemon>(readTask);
+                    id = pk.id;
+                }
+            }
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://pokeapi.co/api/v2/");
+                //HTTP GET
+                var responseTask = client.GetAsync("pokemon-species/" + id);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = await result.Content.ReadAsStringAsync();
+                    //name = readTask;
+                    PokemonSpecies species = JsonSerializer.Deserialize<PokemonSpecies>(readTask);
+
+                    foreach (FlavorTextEntry entry in species.flavor_text_entries)
+                    {
+                        if (entry.language.name.ToLower() == textLanguage)
+                        {
+                            des = entry.flavor_text;
+                        }
+                    }
+                }
+            }
+            //into service, clean string!
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.funtranslations.com/translate/");
+                //HTTP Post
+
+                var json = JsonSerializer.Serialize(des);
+
+
+                var content = new FormUrlEncodedContent(new[]
+                     {
+                new KeyValuePair<string, string>("text", des)
+            });
+
+                var responseTask = client.PostAsync("shakespeare.json/", content);
+
+                responseTask.Wait();
+
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = await result.Content.ReadAsStringAsync();
+                    TranslatedText translation = JsonSerializer.Deserialize<TranslatedText>(readTask);
+                    if (translation.contents.translation.ToLower() == "shakespeare")
+                    {
+                        des = translation.contents.translated;
+                    }
+                }
+            }
+            return des;
+        }
+    }
+}
---
+++ PokemonToShakespeareAPI/Pokemon.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace PokemonToShakespeareAPI
+{
+    [DataContract]
+    public class Pokemon
+    {
+        [DataMember(Name = "id")]
+        public int id { get; set; }
+    }
+    [DataContract]
+    public class Language
+    {
+        [DataMember(Name = "name")]
+        public string name { get; set; }
+        [DataMember(Name = "url")]
+        public string url { get; set; }
+
+    }
+
+    [DataContract]
+    public class FlavorTextEntry
+    {
+        [DataMember(Name = "flavor_text")]
+        public string flavor_text { get; set; }
+        [DataMember(Name = "language")]
+        public Language language { get; set; }
+
+        [DataMember(Name = "version")]
+        public Version version { get; set; }
+      
+    }
+    [DataContract]
+    public class PokemonSpecies
+    {
+        [DataMember(Name = "flavor_text_entries")]
+        public FlavorTextEntry[] flavor_text_entries { get; set; }
+      
+    }
+    [DataContract]
+    public class Success
+    {
+
+        [DataMember(Name = "total")]
+        public int total { get; set; }
+    }
+
+    [DataContract]
+    public class Contents
+    {
+
+        [DataMember(Name = "translated")]
+        public string translated { get; set; }
+
+        [DataMember(Name = "text")]
+        public string text { get; set; }
+
+        [DataMember(Name = "translation")]
+        public string translation { get; set; }
+    }
+
+    [DataContract]
+    public class TranslatedText
+    {
+
+        [DataMember(Name = "success")]
+        public Success success { get; set; }
+
+        [DataMember(Name = "contents")]
+        public Contents contents { get; set; }
+    }
+}
+    
